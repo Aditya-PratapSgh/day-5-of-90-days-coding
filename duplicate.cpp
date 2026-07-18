d24e592566675502b63f289c6f2c74a3aa44018d
Tushar Chauhan <hajikyahalhai72@gmail.com>
2026-07-18 22:41:34 +0530

    Added stack problems and fixed bugs

---
+++ duplicate.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+bool isduplicate(string str){
+    stack<char> s;
+
+    for(int i=0; i<str.size(); i++){
+        
+    }
+}
+
+int main(){
+    string str1="((a+b))";
+    string str2="((a+b)+(c+d))";
+
+
+}
